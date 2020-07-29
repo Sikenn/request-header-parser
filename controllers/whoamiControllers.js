9ec698bb3386cd10e603f7019e7822b495b816a8
Siken <sikenn@pm.me>
2020-07-29 16:35:51 +0200

    ip adress debuged

--- controllers/whoamiControllers.js
+++ controllers/whoamiControllers.js
@@ -1,5 +1,6 @@
 module.exports = whoami = (req, res) => {
-  const ip = req.ip;
+  const ip = req.header('x-forwarded-for') ||
+    req.connection.remoteAddress;
   const languages = req.get('Accept-language');
   const agent = req.get('user-agent')
   res.json({ip, languages, agent})
