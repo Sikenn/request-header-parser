13a92fe1a09a3aab259ad14f527227883adcd1b5
Siken <sikenn@pm.me>
2020-07-29 16:42:23 +0200

    wrong key name

--- controllers/whoamiControllers.js
+++ controllers/whoamiControllers.js
@@ -1,7 +1,7 @@
 module.exports = whoami = (req, res) => {
-  const ipadress = req.header('x-forwarded-for') ||
+  const ipaddress = req.header('x-forwarded-for') ||
     req.connection.remoteAddress;
   const language = req.get('Accept-language');
   const software = req.get('user-agent')
-  res.json({ ipadress, language, software })
+  res.json({ ipaddress, language, software })
 }
