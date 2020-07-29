afcec87022d9c0391b2f2b711f823feb4d3fbe31
Siken <sikenn@pm.me>
2020-07-29 16:40:57 +0200

    the key in the res.json is now correct

--- controllers/whoamiControllers.js
+++ controllers/whoamiControllers.js
@@ -1,7 +1,7 @@
 module.exports = whoami = (req, res) => {
-  const ip = req.header('x-forwarded-for') ||
+  const ipadress = req.header('x-forwarded-for') ||
     req.connection.remoteAddress;
-  const languages = req.get('Accept-language');
-  const agent = req.get('user-agent')
-  res.json({ip, languages, agent})
+  const language = req.get('Accept-language');
+  const software = req.get('user-agent')
+  res.json({ ipadress, language, software })
 }
