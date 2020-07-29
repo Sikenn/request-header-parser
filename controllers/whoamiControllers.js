4f7aa09f66623d898e46bf884160213b6b52a504
Siken <sikenn@pm.me>
2020-07-29 16:29:42 +0200

    Adding the ip

--- controllers/whoamiControllers.js
+++ controllers/whoamiControllers.js
@@ -1,5 +1,5 @@
 module.exports = whoami = (req, res) => {
-  const ip = req.get('host');
+  const ip = req.ip;
   const languages = req.get('Accept-language');
   const agent = req.get('user-agent')
   res.json({ip, languages, agent})
